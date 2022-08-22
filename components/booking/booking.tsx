9f82ec593ebf6662b43b61c058a05b2aedf2e40a
BenjiA123 <adeoul001@gmail.com>
2022-08-22 09:17:27 -0700

    Updates

--- components/booking/booking.tsx
+++ components/booking/booking.tsx
@@ -8,14 +8,19 @@ const BookingComponent:FunctionComponent = ()=>{
     return (
         <div>
             <>
-            <section className={styles.booking}>
-                <div className="">
+            <section  className={styles.booking_section}>
+            <h2 className={styles.booking_header}>BOOK A TICKET NOW</h2>
+
+            <div className={styles.booking}>
+                
+               <div className={styles.booking_form}>
+               <div className="">
                     <span>Book Date</span><br />
                <input type="text" />
                 </div>
                <div className="">
-                <span>Book Date</span><br />
-               <input type="text" />
+                <span>Return Date</span><br />
+               <input type="date" />
                </div>
                <div className="">
                 <span>Book Date</span><br />
@@ -23,12 +28,27 @@ const BookingComponent:FunctionComponent = ()=>{
                </div>
                <div className="">
                 <span>Book Date</span><br />
-               <input type="text" />
+               <input type="date" />
                </div>
                <div className="">
-                <span>Book Date</span><br />
-               <input type="text" />
+                <span>Select Category</span><br />
+               <select name="" id="">
+                <option value="">Luxury</option>
+                <option value="">Mid-Class</option>
+                <option value="">Budget</option>
+               </select>
                </div>
+               <div className="">
+               <button className={styles.getCar} > Get A Car</button>
+               </div>
+               </div>
+               <div className={styles.booking_info}>
+                
+                <h2>Use Our Fast And Reliable Service and Get Served In Minutes</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, 
+                    fugit odio, sunt voluptas quisquam saepe itaque sequi a. Iusto.</p>
+               </div>
+            </div>
             </section>
             </>
         </div>
